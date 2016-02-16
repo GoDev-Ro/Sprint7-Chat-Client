d68ae5df007e416fdde0ab89c623858d973e49d6
oportocala <oportocala@gmail.com>
2016-02-16 16:56:11 +0200

    Fat arrow

--- src/components/ActiveUsers.js
+++ src/components/ActiveUsers.js
@@ -10,16 +10,15 @@ var ActiveUsers = React.createClass({
 
   render: function () {
 
-    var activeUsers = this.props.list.map(function (user, idx) {
-      return (<User key={idx} fb_id={user.fb_id} firstName={user.first_name} lastName={user.last_name} />);
-    });
+    var activeUsers = this.props.list.map(
+       (user, idx) => (<User key={idx} fb_id={user.fb_id} firstName={user.first_name} lastName={user.last_name} />)
+    );
 
     return (
       <div className="active-users">
         {activeUsers}
       </div>
     );
-  }
 });
 
 export default ActiveUsers;
